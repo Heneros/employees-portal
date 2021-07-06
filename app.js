992f9e9efacee95046c13fc015f97213bedf7945
Heneros <punk60826@gmail.com>
2021-07-07 02:42:05 +0300

    add and delete users

--- app.js
+++ app.js
@@ -31,3 +31,53 @@ let fetchAllEmployees = () => {
      document.querySelector('#table-body').innerHTML = employeeRows;
     });
 }
+
+
+let addEmployeeForm = document.querySelector('#add-employee-form');
+addEmployeeForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    $('#add-employee-form').modal('hide');
+    let employee = {
+        first_name: document.querySelector('#add-first-name').value,
+        last_name: document.querySelector('#add-last-name').value,
+        email: document.querySelector('#add-email').value,
+        gender: document.querySelector('#add-gender').value,
+        ip_address: document.querySelector('#add-ip-address').value,
+    };
+    let url = `${serverURL}/employees`;
+    let http = new BrainHttp();
+    http.post(url, employee, (data) => {
+        console.log(data);
+        fetchAllEmployees();
+        clearFormFields();
+    });
+
+})
+
+
+let clearFormFields = () =>{
+        document.querySelector('#add-first-name').value = '';
+        document.querySelector('#add-last-name').value = '';
+        document.querySelector('#add-email').value = '';
+        document.querySelector('#add-gender').value = '';
+        document.querySelector('#add-ip-address').value = '';
+}
+
+let tableBody = document.querySelector('#table-body');
+tableBody.addEventListener('click', function(e){
+    let targetElement = e.target;
+    if(targetElement.classList.contains('delete')){
+        let selectedId = targetElement.parentElement.parentElement.firstElementChild.innerHTML;
+        let url = `${serverURL}/employees/${selectedId}`;
+        let http = new BrainHttp();
+        http.delete(url, (data) =>{
+            console.log(data);
+            fetchAllEmployees();
+        })
+      
+    }
+    if(targetElement.classList.contains('update')){
+        console.log('You clicked update');
+    }
+
+})
